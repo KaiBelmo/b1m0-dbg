fbb12b83175511dd39df83a93b6ffe3c3f24474e
Collin Belmo <51862296+CollinBelmo@users.noreply.github.com>
2020-12-16 00:00:37 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,135 +1,114 @@
-/*
-	Spaghetti code!
-*/
 
-#include "inc/parser.h"
-#include "inc/dbg.h"
+#include "breakpoint.h"
+#include "dbg.h"
+#include "elf_parser.h"
+#include "proc.h"
 
-struct user_regs_struct regs;
-h_cmd			*h_command = NULL;
-break_p			*break_point = NULL;
+breakpoint *breakpoint_list = NULL;
+commandline *commandline_list = NULL;
 
-int	main(int argc, char** argv){
+int main(int argc, char **argv) {
+    child_proc *proc = (child_proc *)malloc(sizeof(child_proc));
+    uint64_t break_address;
+    int wstatus;
 
-	Elf64_Ehdr	header;
-	Elf64_Shdr* shdr;
+    if (argc != 2) {
+        f_usage(argv[0]);
+        return -1;
+    }    
+    if (proc == NULL) {
+        perror("malloc");
+        return 1;
+    }
+    memset(proc, 0, sizeof(child_proc));
+    proc->fd = open(argv[1], O_RDONLY | O_SYNC);
+    proc->command = (char *)malloc(sizeof(char) * 14);
 
-	char		 address[17];
-	char		 *prog;
-	int			 status = {0};
-	unsigned long long int break_address;
-	unsigned long long int instruction;
-	int32_t		 fd;
-	pid_t		 pid;
-	shdr = 0;
+    if (f_check(argv[1])) {
+        return -1;
+    }
+    if (!(f_check_elf(proc->fd, (&proc->header)))) {
+        printf("Reading info from executable\n");
+    } else {
+        printf("\t Check your executable!\n");
+        return -1;
+    }
 
-	prog	= argv[1];
-	//prog = "asdf.out";
-	fd		= open(prog, O_RDONLY|O_SYNC);
-	char cmd[15];
-
-	if (argc < 2){
-		f_usage(argv[0]);
-		return -1;
-	}
-	if (f_check(prog)){
-		return -2;
-	}
-	if (!(f_check_elf(fd, &header))){
-		printf("Reading info from executable\n");
-	}
-	else {
-		printf("\t Check your executable!\n");
-		return -3;
-	}
-
-	pid	= fork();
-	if (pid == -1){
-		perror ("fork");
-		return -4;
-	}
-	if (pid == 0) {
-		child_process(prog);
-	}
-     	else if (pid >= 1)  {
-		usleep(200000);
-		printf("Attaching to pid %d\n", pid); 
-		waitchild(pid);
-		while(1){
-			printf("\x1B[31;1m[B1MO:0x%llx]>\x1B[0m", regs.rip);
-			fgets(cmd, 14, stdin);
-			insert_cmd(&h_command, cmd);
-			strtok(cmd, "\n");
-			fflush(stdin);
-			switch(handle_cmd(cmd)){
-				case 0:
-					single_step(pid, &regs);
-					break;
-				case 1:
-					print_cmd(h_command);
-					break;
-				case 2:
-					//info header / info section / info symbols
-					if(strcmp(cmd, "info file") == 0){
-						info_header(header);
-					}
-					else if(strcmp(cmd, "info section") == 0){
-						print_section(header, shdr, fd);
-					}
-					else if(strcmp(cmd, "info symbols") == 0){
-						print_symbols(header, shdr, fd);
-						// laateeeer
-					}
-					break;
-				case 3:
-					// kill the child :(
-					kill(pid, SIGKILL);
-					goto EXIT;
-					break;
-				case 4:
-					info_regs(&regs, pid);
-					break;
-				case 5:
-					cont_step(pid);
-					//err = ptrace(PTRACE_CONT, pid, NULL, NULL);
-					wait(&status);
-					goto EXIT;
-					break;
-				case 6:
-					trace_syscall(pid);
-					break;
-				case 7:
-					step_syscall(pid, &regs);
-					break;
-				case 8:
-					printf("address: ");
-					fgets(address, 16, stdin);
-					break_address = (unsigned long long int)strtol(address, NULL, 16);
-					add_break_point(&break_point, break_address);
-					instruction = set_break_p(pid, break_address);  
-					ptrace(PTRACE_CONT, pid, 0, 0);
-					wait(&status);
-					if (WIFSTOPPED(status)) {
-						ptrace(PTRACE_POKETEXT, pid, address, instruction);
-					}
-					ptrace(PTRACE_GETREGS, pid, NULL, &regs);
-					regs.rip = break_address;
-					set_ins(pid, break_address, instruction);
-					ptrace(PTRACE_SETREGS, pid, NULL, &regs);
-					// i will fix it laterrrr
-					break;
-				case 9:
-					print_break_points(break_point);
-					break;
-				case 10:
-					print_memory(pid);
-					break;
-				}
-				//strcpy(cmd, "");
-		}
-	}
-	EXIT:
-	printf("Exit...");
-
-	return 0;
+    proc->pid = fork();
+    if (proc->pid == -1) {
+        perror("fork");
+        return -1;
+    }
+    if (proc->pid == 0) {
+        child_process(argv[1]);
+    } else if (proc->pid >= 1) {
+        usleep(200000);
+        printf("Attaching to pid %d\n", proc->pid);
+        wait(&wstatus);
+        while (1) {
+            printf(RED "[B1MO:0x%llx]>" NORMAL, proc->regs.rip);
+            proc->command = command_input();
+            insert_cmd(&commandline_list, proc->command);
+            fflush(stdin);
+            switch (handle_cmd(proc->command)) {
+                case 0:
+                    single_step(proc->pid, (&proc->regs));
+                    break;
+                case 1:
+                    print_cmd(commandline_list);
+                    break;
+                case 2:
+                    // info header / info section / info symbols
+                    if (strcmp(proc->command, "info file") == 0) {
+                        info_header(proc->header);
+                        break;
+                    } else if (strcmp(proc->command, "info section") == 0) {
+                        print_section(proc->header, proc->shdr, proc->fd);
+                        break;
+                    } else if (strcmp(proc->command, "info symbols") == 0) {
+                        //print_symbols(proc->header, proc->shdr, proc->fd);
+                        unimplemented();
+                        break;
+                    }
+                    break;
+                case 3:
+                    // uwu
+                    ptrace(PTRACE_KILL, proc->pid, NULL, NULL);
+                    goto EXIT;
+                    break;
+                case 4:
+                    info_regs((&proc->regs), proc->pid);
+                    break;
+                case 5:
+                    continue_exec(&proc, &breakpoint_list);
+                    if (proc->running ==  1){
+                        goto EXIT;
+                    }   
+                    break;
+                case 6:
+                    trace_syscall(proc->pid);
+                    goto EXIT;
+                case 7:
+                    step_syscall(&breakpoint_list, &proc);
+                    break;
+                case 8:
+                    if ((break_address = set_address(proc->command)) == 1) {
+                        printf(WHITE "Check your input\n" NORMAL);
+                        break;
+                    }
+                    set_breakpoint_at_address(proc->pid, break_address, &breakpoint_list);
+                    break;
+                case 9:
+		    print_breakpoint_list(breakpoint_list);
+                    break;
+                case 10:
+                    print_memory(proc->pid);
+                    break;
+                default:
+                    printf(WHITE"input:\x1B[0m %s command not found\n", proc->command);
+            }
+        }
+    } 
+EXIT:
+    return 0;
 }
